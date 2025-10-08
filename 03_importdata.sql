0f93c18df783dca28f6c9fbe1e379f5f179d1dff
Idamin Rahaman <100356798+idaminrahaman@users.noreply.github.com>
2025-10-08 16:39:23 +0530

    Update 03_importdata.sql

--- 03_importdata.sql
+++ 03_importdata.sql
@@ -1,10 +1,10 @@
--- Active: 1756533320549@@localhost@3306@used_car_sales_db
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/BMW_sales_data_(2010-2024).csv'
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/car_resale_prices.csv'
 INTO TABLE sales_analysis
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"' 
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
-(Model, Year, Region, Color, Fuel_Type, Transmission, Engine_Size_L, Mileage_KM, Price_USD, Sales_Volume, Sales_Classification);
+(id,full_name,resale_price,registered_year,engine_capacity,insurance,transmission_type,kms_driven,owner_type,fuel_type,max_power,seats,mileage,body_type,city
+);
 
 SELECT * FROM sales_analysis LIMIT 10;
